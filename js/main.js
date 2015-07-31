f613f61a625b70da3d34f0307709c4be3fe5accd
amerriman <amm434@gmail.com>
2015-07-31 13:33:31 -0600

    needs to be refactored, but is done and commented out for later

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ $(document).ready(function(){
   $('.operator').not('#cancel').not("#calc").on("click", function(){
     var opPressed = $(this).html();
     $('#screen').append(" " + opPressed + " ");
+
   });
 
   //clear the screen when C is clicked
@@ -19,7 +20,7 @@ $(document).ready(function(){
     $('#screen').html(null);
   });
 
-//when calc is clicked, clear the html from the screen - and eventually replace with the answer
+//when calc is clicked, clear the html from the screen - run the massive function and append the answer to the screen
   $('#calc').on("click", function(){
     //This is where the magic happens
     //var total = eval($('#screen').html());
@@ -27,30 +28,42 @@ $(document).ready(function(){
     $('#screen').html(null).append(total);
   });
 
+//all the operators html
   var operators = ["+", "-", "*", "/"];
 
   function getScreen(str){
-  var screenResults = $('#screen').html().split(" ");
-  var a = parseInt(screenResults[0]);
-  var b = parseInt(screenResults[2]);
+    //variable that grabs the screen, and splits it so it becomes an array
+    var screenResults = $('#screen').html().split(" ");
+    console.log(screenResults);
+    //assign variables to the number strings at positions 0 and 2 to use later in the calculations
+    var a = parseInt(screenResults[0]);
+    var b = parseInt(screenResults[2]);
+
+      //loop through the operators array AND the newly created screenResults array to find which operator is being used.
       for (var i = 0; i < screenResults.length; i++) {
         for (var j = 0; j < operators.length; j++) {
+          //when the operator is found
           if (screenResults[i] === operators[j]){
           console.log("opj", operators[j]);
+          //assign the operator to 'math'
           math = operators[j];
           console.log(math, "math");
           //return math;
+          //if math matches the operator at index 0 in the operators array, then run the funtion 'addition' with variables a and b as created above. Etc for each operator index.
           if(math === operators[0]){
-             total = add(a, b);
+            total = addition(a, b);
           }
           else if(math === operators[1]){
                total = subtract(a, b);
+               //total = total.toFixed(5);
             }
           else if(math === operators[2]){
                total = multiply(a, b);
+               total = total.toFixed(2);
             }
           else if(math === operators[3]){
                total = divide(a, b);
+               total = total.toFixed(2);
           }
         }
       }
@@ -65,9 +78,9 @@ $(document).ready(function(){
 
 
 
+//basic math functions
 
-
-function add (a, b){
+function addition (a, b){
    var result = a + b;
    return result;
   }
@@ -87,6 +100,7 @@ function add (a, b){
     return result;
   }
 
+});
 
         //find the index of +
 //          var index = screenResults.indexOf("+");
@@ -136,11 +150,6 @@ function add (a, b){
 
 
 
-
-
-
-
-});
 
 //unbelievable.
 // --sample = "8+2*9"
